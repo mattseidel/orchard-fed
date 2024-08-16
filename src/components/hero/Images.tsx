ee44c662fa478945d3a32543cb0cd196582d3966
Matthew Seidel <matseiw@gmail.com>
2024-08-16 18:16:44 -0500

    TODO: add Images

--- src/components/hero/Images.tsx
+++ src/components/hero/Images.tsx
@@ -1,6 +1,7 @@
 import { images } from "./ImagesList";
 import ProgressiveImage from "../common/Image";
 
+// TODO: improve the aspect ratio of the images on md and lg screens
 export const ImagesBody = () => (
   <section className="grid grid-cols-1 xl:grid-cols-2 gap-6 col-span-2 order-last xl:-order-last">
     <div className="h-[550px] xl:h-full">

b404b72d82c3b4c84dce9f9ef1b2ce047a547c02
Matthew Seidel <matseiw@gmail.com>
2024-08-19 20:19:48 -0500

    style: fix screen minor issue

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Hero } from "./components/hero/Hero";
 
 const App = () => {
   return (
-    <main className="bg-black min-h-screen px-4 py-2 lg:px-32 xl:py-44 md:px-20 md:py-10">
+    <main className="bg-black min-h-screen px-4 py-2 xl:px-32 xl:py-44 md:px-20 md:py-10">
       <Hero />
       <Container />
       
